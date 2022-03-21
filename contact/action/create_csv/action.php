4a2180063caf792b8b8a2a652384e29d041787c1
k-sugisaki <jianaizishanqi676@gmail.com>
2022-03-21 19:34:38 +0900

    move: action.phpの移動

--- contact/action/create_csv/action.php
+++ contact/action/create_csv/action.php
@@ -2,24 +2,24 @@
 <?php
 
 $data = [
-    array(
-        'corp_name' => '法人名',
-        'tel' => '電話番号',
-        'category' => '会員区分',
-        'name' => '参加者名',
-        'name_kana' => 'フリガナ',
-        'mail' => 'メールアドレス',
-    ),
-    array(
-        'corp_name' => $corp_name,
-        'tel' => $tel,
-        'category' => $category,
-        'name' => $name,
-        'name_kana' => $name_kana,
-        'mail' => $mail,
-        )
+  array(
+    'corp_name' => '法人名',
+    'tel' => '電話番号',
+    'category' => '会員区分',
+    'name' => '参加者名',
+    'name_kana' => 'フリガナ',
+    'mail' => 'メールアドレス',
+  ),
+  array(
+    'corp_name' => $corp_name,
+    'tel' => $tel,
+    'category' => $category,
+    'name' => $name,
+    'name_kana' => $name_kana,
+    'mail' => $mail,
+  )
 ];
- 
+
 $save_path = "sample.csv";
 
 /**
@@ -33,23 +33,23 @@ function create_csv($data, $save_path)
 
   //一時データを開く
   $fp = fopen('php://temp', 'r+b');
- 
+
   //fputcsvでCSVデータを作る
   foreach ($data as $val) {
     fputcsv($fp, $val);
   }
- 
+
   //ファイルポインタを先頭に戻す
   rewind($fp);
- 
+
   //ストリームの中身をテキストデータに変換、
   //さらにテキストデータをUTF-8からSJIS-winに変換する
   $str = str_replace(PHP_EOL, "\r\n", stream_get_contents($fp));
   $str = mb_convert_encoding($str, 'SJIS-win', 'UTF-8');
- 
+
   //一時データのファイルポインタを閉じる
   fclose($fp);
- 
+
   //CSVファイルを生成して、データを書き込んで保存する
   $fp2 = fopen($save_path, "w");
   fwrite($fp2, $str);
@@ -57,4 +57,3 @@ function create_csv($data, $save_path)
 }
 
 create_csv($data, $save_path);
-?>
