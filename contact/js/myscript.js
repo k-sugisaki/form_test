9df3bd3c0d0080f93f43758b05f21e2e2dade645
makiikuta <mk0629bb@gmail.com>
2022-03-30 17:18:16 +0900

    add:うまくいっていない

--- contact/js/myscript.js
+++ contact/js/myscript.js
@@ -16,3 +16,12 @@ $('.representative:nth-child(n+2)').remove();
 // 代表者のみの入力項目を削除（法人名・電話番号・会員or一般）
 // うまくいっていないので相談する
 $('.primary_only:nth-child(n+2)').remove();
+
+// 二人目以降の参加者にクラスを追加（削除ボタンをつけるため）
+// const REMOVE = document.getElementsByClassName("entrant");
+// REMOVE[1].classList.add("off");
+// パターン２（どちらもうまくいっていない）
+// const columns = document.querySelectorAll('.entrant');
+// for (let i = 2; i < columns.length; i++) {
+//     columns[i].classList.add('off');
+}
