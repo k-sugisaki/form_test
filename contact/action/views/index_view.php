7fc3b1e85b6e13a2ec526554d508c46842d16a9b
k-sugisaki <jianaizishanqi676@gmail.com>
2022-05-24 21:33:54 +0900

    電話番号のハイフン削除js

--- contact/action/views/index_view.php
+++ contact/action/views/index_view.php
@@ -163,6 +163,7 @@
   </main>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/contact/js/add_participant.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/contact/js/validation.js"></script>
 </body>
 
 </html>
