1a7a37f70dafedc4afa489ea21652ec26038e49d
k-sugisaki <jianaizishanqi676@gmail.com>
2022-06-15 23:36:58 +0900

    jsエラー修正

--- contact/js/validation.js
+++ contact/js/validation.js
@@ -1,12 +1,12 @@
 //電話番号入力
-const telInput = document.getElementById('tel');
-
-telInput.addEventListener('blur', ()=>{
-  let telInputValue = telInput.value;
-  telInputValue = telInputValue.replace(/[-ー―‐－]/g, '');
-  const replacedValue = telInputValue.replace(/[０-９]/g,
-  function(str){
-    return String.fromCharCode(str.charCodeAt(0) - 0xFEE0)
-  });
-  telInput.value = replacedValue;
-}, false);
+const telInput = document.getElementById("tel");
+if (telInput) {
+  telInput.addEventListener("blur",() => {
+    let telInputValue = telInput.value;
+    telInputValue = telInputValue.replace(/[-ー―‐－]/g, "");
+    const replacedValue = telInputValue.replace(/[０-９]/g, function (str) {
+      return String.fromCharCode(str.charCodeAt(0) - 0xfee0);
+    });
+    telInput.value = replacedValue;
+  }, false);
+}
